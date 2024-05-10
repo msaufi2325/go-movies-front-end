ea8c0d6c7c87306cb3f1a48c23d06ebb09cea993
Saufi <msaufi2325@gmail.com>
2024-05-10 23:58:56 +0900

    chore: Update index.html and App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div>
-      <h1>Go Watch Movies</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
