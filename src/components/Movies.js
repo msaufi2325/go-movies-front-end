62dff51347b2df6c1773e834970208494bd68920
Saufi <msaufi2325@gmail.com>
2024-05-11 11:55:47 +0900

    Add Movie component and update navigation links

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -39,7 +39,7 @@ const Movies = () => {
   }, []);
 
   return (
-    <div className="text-center">
+    <div>
       <h2>Movies</h2>
       <hr />
       <table className="table table-striped table-hover">
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import EditMovie from "./components/EditMovie";
 import ManageCatalogue from "./components/ManageCatalogue";
 import GraphQL from "./components/GraphQL";
 import Login from "./components/Login";
+import Movie from "./components/Movie";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "/movies", element: <Movies /> },
+      { path: "/movies/:id", element: <Movie /> },
       { path: "/genres", element: <Genres /> },
       { path: "/admin/movie/0", element: <EditMovie /> },
       { path: "/manage-catalogue", element: <ManageCatalogue /> },
