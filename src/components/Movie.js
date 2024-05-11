f3779119a9940fc3414798caa99ede3693fa41c7
Saufi <msaufi2325@gmail.com>
2024-05-11 12:40:14 +0900

    Add movie details to Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Movie = () => {
+  const [movie, setMovie] = useState({});
+  let { id } = useParams();
+
+  useEffect(() => {
+    let myMovie = {
+      id: 1,
+      title: "The Shawshank Redemption",
+      release_date: "1994-10-14",
+      runtime: 142,
+      mpaa_rating: "R",
+      description:
+        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    };
+    setMovie(myMovie);
+  }, [id]);
   return (
-    <div className="text-center">
-      <h2>Movie</h2>
+    <div>
+      <h2>Movie: {movie.title}</h2>
+      <small>
+        <em>
+          {movie.release_date}, {movie.runtime} minutes, Rated{" "}
+          {movie.mpaa_rating}
+        </em>
+      </small>
       <hr />
+      <p>{movie.description}</p>
     </div>
   );
 };
