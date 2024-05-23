7b1b96e235284da4d5abaa7a94ec2cdca239a2aa
Saufi <msaufi2325@gmail.com>
2024-05-24 06:54:58 +0900

    Add API authentication to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,17 +14,37 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("email/password", email, password);
 
-    if (email === "admin@example.com") {
-      setJwtToken("abc");
-      setAlertClassName("d-none");
-      setAlertMessage("");
-      navigate("/");
-    } else {
-      setAlertClassName("alert-danger");
-      setAlertMessage("Invalid email or password");
-    }
+    // build the request payload
+    let payload = {
+      email: email,
+      password: password,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(payload),
+    };
+
+    fetch(`/authenticate`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          setAlertClassName("alert-danger");
+          setAlertMessage(data.message);
+        } else {
+          setJwtToken(data.token);
+          setAlertClassName("d-none");
+          setAlertMessage("");
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        setAlertClassName("alert-danger");
+        setAlertMessage(error);
+      });
   };
 
   return (
