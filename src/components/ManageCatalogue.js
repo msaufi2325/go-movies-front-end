ff09f2d261cbb660450620031e48a5aff32bd809
Saufi <msaufi2325@gmail.com>
2024-05-30 23:46:57 +0900

    Update ManageCatalogue component to fetch and display movies from API

--- src/components/ManageCatalogue.js
+++ src/components/ManageCatalogue.js
@@ -1,8 +1,58 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+
 const ManageCatalogue = () => {
+  const [movies, setMovies] = useState([]);
+  const { jwtToken } = useOutletContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      navigate("/login");
+      return;
+    }
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    };
+
+    fetch(`http://localhost:8080/movies`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((err) => console.error(err));
+  }, [jwtToken, navigate]);
+
   return (
-    <div className="text-center">
-      <h2>Manage Catalogue</h2>
+    <div>
+      <h2>Movies</h2>
       <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => {
+            return (
+              <tr key={movie.id}>
+                <td>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </td>
+                <td>{movie.release_date}</td>
+                <td>{movie.mpaa_rating}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
