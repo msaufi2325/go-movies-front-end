4335230bf8ec52a5c5b88790ce12ac7ad63c9029
msaufi2325 <msaufi2325@gmail.com>
2024-06-09 10:01:17 +0900

    Refactor EditMovie component to optimize form submission and error handling

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -94,6 +94,27 @@ const EditMovie = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let errors = [];
+    let required = [
+      { field: movie.title, name: "title"},
+      { field: movie.release_date, name: "release_date"},
+      { field: movie.runtime, name: "runtime"},
+      { field: movie.mpaa_rating, name: "mpaa_rating"},
+      { field: movie.description, name: "description"},
+    ]
+
+    required.forEach(function (obj) {
+      if (obj.field === "") {
+        errors.push(obj.name);
+      }
+    })
+
+    setErrors(errors);
+
+    if (errors.length > 0) {
+      return false;
+    }
   };
 
   const handleChange = () => (e) => {
@@ -208,6 +229,10 @@ const EditMovie = () => {
             ))}
           </>
         )}
+
+        <hr />
+
+        <button className="btn btn-primary">Save</button>
       </form>
     </div>
   );
